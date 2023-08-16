723c5ab9732c360d0eb9fa34104ea9485fc36383
valmon55 <fedor_ka@mail.ru>
2023-08-16 18:06:12 +0300

    linked list performance

---
+++ Collection/Collection.LinkedListPerformance/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+
+namespace Collection.LinkedListPerformance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            LinkedList<string> list = new LinkedList<string>();
+
+            stopWatch.Start();
+
+            string file = @"D:\Temp\13_6_1_Text.txt";
+
+            if (File.Exists(file))
+            {
+                using (StreamReader sr = File.OpenText(file))
+                {
+                    string s = sr.ReadToEnd();
+                    char[] delimiters = { ' ', '\r', '\n' };
+                    string[] w = s.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+                    foreach (string word in w)
+                        list.AddFirst(word);
+
+                }
+            }
+
+            stopWatch.Stop();
+            Console.WriteLine($"Прошло {stopWatch.Elapsed.Milliseconds} миллисек.");
+
+            Console.ReadKey();
+        }
+    }
+}
