6cad4b75d1945821e7564ee3afd0762e2ecb98d8
valmon55 <fedor_ka@mail.ru>
2023-08-16 17:42:58 +0300

    10 words

---
+++ Collection/Collection.TenWords/WordCount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection.TenWords
+{
+    public class WordCount
+    {
+        public string word;
+        public int number;
+
+        public WordCount(string word, int number)
+        {
+            this.word = word;
+            this.number = number;
+        }
+    }
+}
